9524f3f7162ef251950731e341c6594a9c8c2922
jsrmalvarez <jsrmalvarez@gmail.com>
2024-12-09 15:28:18 +0100

    Can expand data

--- src/day09/day09.zig
+++ src/day09/day09.zig
@@ -2,50 +2,20 @@ const std = @import("std");
 const FREE_BLOCK = '.';
 
 test "data expansion" {
-    const data = "2333133121414131402";
-    var list: std.DoublyLinkedList(u8) = undefined;
-    var listElementCount: usize = 0;
-
-    for (data, 0..data.len) |c, n| {
-        const id = @as(u8, @intCast(n));
-        const num = try std.fmt.charToDigit(c, 10);
-        if (n % 2 == 0) {
-            // num is number of file blocks
-            for (0..num) |_| {
-                const newNode = std.testing.allocator.alloc(std.DoublyLinkedList(u8).Node{ .data = id }, 1);
-                list.append(newNode);
-                listElementCount += 1;
-            }
-        } else {
-            // num is number of free blocks
-            for (0..num) |_| {
-                list.append(FREE_BLOCK);
-                listElementCount += 1;
-            }
-        }
-    }
-
-    var string = [_]u8{undefined} ** 256;
-    for (list, 0..listElementCount) |e, n| {
-        string[n] = e;
-    }
-
-    try std.testing.expect(true);
-}
-pub fn getResultDay09_1(allocator: std.mem.Allocator) !u64 {
-    var arena = std.heap.ArenaAllocator.init(allocator);
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
     var arenaAllocator = arena.allocator();
 
     const data = "2333133121414131402";
-    const listArray: []std.DoublyLinkedList(u8) = try allocator.alloc(std.DoublyLinkedList(u8), 1);
-    var list = &listArray[0];
-    defer allocator.free(listArray);
+    const U8List = std.DoublyLinkedList(u8);
+    var list = U8List{};
+
+    var string = [_]u8{undefined} ** 256;
 
     var listElementCount: usize = 0;
-
+    var id: u8 = 0;
+    var string_index: usize = 0;
     for (data, 0..data.len) |c, n| {
-        const id = @as(u8, @intCast(n));
         const num = try std.fmt.charToDigit(c, 10);
         if (n % 2 == 0) {
             // num is number of file blocks
@@ -54,7 +24,10 @@ pub fn getResultDay09_1(allocator: std.mem.Allocator) !u64 {
                 newNode[0].data = id;
                 list.append(&newNode[0]);
                 listElementCount += 1;
+                string[string_index] = id + '0';
+                string_index += 1;
             }
+            id += 1;
         } else {
             // num is number of free blocks
             for (0..num) |_| {
@@ -62,14 +35,50 @@ pub fn getResultDay09_1(allocator: std.mem.Allocator) !u64 {
                 newNode[0].data = FREE_BLOCK;
                 list.append(&newNode[0]);
                 listElementCount += 1;
+                string[string_index] = FREE_BLOCK;
+                string_index += 1;
             }
         }
     }
+    std.debug.print("|{s}|\n", .{string[0..string_index]});
+    try std.testing.expect(std.mem.eql(u8, "00...111...2...333.44.5555.6666.777.888899", string[0..string_index]));
+}
+pub fn getResultDay09_1(allocator: std.mem.Allocator) !u64 {
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    var arenaAllocator = arena.allocator();
 
-    var string = [_]u8{undefined} ** 256;
-    for (0..listElementCount) |n| {
-        if (list.pop()) |node| {
-            string[n] = node.data;
+    const data = "2333133121414131402";
+    //const listArray: []std.DoublyLinkedList(u8) = try allocator.alloc(std.DoublyLinkedList(u8), 1);
+    //var list = &listArray[0];
+    //defer allocator.free(listArray);
+    const U8List = std.DoublyLinkedList(u8);
+    var list = U8List{};
+
+    var listElementCount: usize = 0;
+    std.debug.print("|", .{});
+    var id: u8 = 0;
+    for (data, 0..data.len) |c, n| {
+        const num = try std.fmt.charToDigit(c, 10);
+        if (n % 2 == 0) {
+            // num is number of file blocks
+            for (0..num) |_| {
+                const newNode: []std.DoublyLinkedList(u8).Node = try arenaAllocator.alloc(std.DoublyLinkedList(u8).Node, 1);
+                newNode[0].data = id;
+                list.append(&newNode[0]);
+                listElementCount += 1;
+                std.debug.print("{c}", .{id + '0'});
+            }
+            id += 1;
+        } else {
+            // num is number of free blocks
+            for (0..num) |_| {
+                const newNode: []std.DoublyLinkedList(u8).Node = try arenaAllocator.alloc(std.DoublyLinkedList(u8).Node, 1);
+                newNode[0].data = FREE_BLOCK;
+                list.append(&newNode[0]);
+                listElementCount += 1;
+                std.debug.print("{c}", .{FREE_BLOCK});
+            }
         }
     }
 
